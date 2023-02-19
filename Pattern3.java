8e688e911786ccfdcfe64704064dbf9da6f20db5
Tushardhiman21 <105099130+Tushardhiman21@users.noreply.github.com>
2023-02-19 13:13:21 +0530

    Add files via upload
    StarPattern3

---
+++ Pattern3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Pattern3 {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            
+            System.out.print("enter the no of rows:- ");
+            int r=sc.nextInt();
+            System.out.print("\nenter the no of columns:- ");
+            int c=sc.nextInt();
+        
+        for(int i = 0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(i==0||i==r-1||j==(c-1)/2)
+                {
+                    System.out.print("*");
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        
+        
+    }
+
+
+}
