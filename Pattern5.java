1e0ee80e45eac049d02158bbd478ded010e88a55
Tushardhiman21 <105099130+Tushardhiman21@users.noreply.github.com>
2023-02-19 13:14:57 +0530

    Add files via upload
    StarPattern5

---
+++ Pattern5.java
@@ -0,0 +1,25 @@
+
+
+public class Pattern5{
+    public static void main(String[] args) {
+        for( int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+            System.out.print("*");
+            }
+            
+          System.out.print(" ");
+        
+    
+            for(int k=0;k<3;k++)
+            {
+            System.out.print("*");
+            }
+        
+
+        System.out.println();
+    }
+    
+    }
+}
